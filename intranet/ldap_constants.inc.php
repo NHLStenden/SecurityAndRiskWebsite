dfed8cb362c2add086c52cd395a51055b881372e
Martin Molema <martin@molema.org>
2020-02-16 16:17:57 +0100

    Een kleine aanwijzing om nog eens te kijken naar plain-text passwords

--- intranet/ldap_constants.inc.php
+++ intranet/ldap_constants.inc.php
@@ -4,4 +4,4 @@ define("BASE_DN", "ou=mijnsite,dc=mijnsite,dc=local");
 define("LDAP_HOST","192.168.123.149");
 define("LDAP_PORT",389);
 define("LDAP_ADMIN_CN","cn=webuserldap,ou=users,". BASE_DN);
-define("LDAP_PASSWORD","wachtwoord");
+define("LDAP_PASSWORD","wachtwoord"); // FIXME: Investigate how to prevent plaintext passwords.
