8ca2dfe038d25385c22c1bc5dbaace8d443ab42c
Martin Molema <martin@molema.org>
2020-02-16 13:58:47 +0100

    Added a constant-file to setup the LDAP connectivity

---
+++ intranet/ldap_constants.inc.php
@@ -0,0 +1,7 @@
+<?php
+// set some constants
+define("BASE_DN", "ou=mijnsite,dc=mijnsite,dc=local");
+define("LDAP_HOST","192.168.123.149");
+define("LDAP_PORT",389);
+define("LDAP_ADMIN_CN","cn=webuserldap,ou=users,". BASE_DN);
+define("LDAP_PASSWORD","wachtwoord");
